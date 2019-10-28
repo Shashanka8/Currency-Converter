fb95c4bdb52944b9b015c7c247d883c4edb6799d
Shashanka Shekhar Nayak <44174922+Shashanka8@users.noreply.github.com>
2019-10-28 18:18:52 +0530

    Updated Pipe

--- src/app/convert.pipe.ts
+++ src/app/convert.pipe.ts
@@ -1,18 +1,28 @@
 import { Pipe, PipeTransform } from "@angular/core";
+import { HttpClient } from '@angular/common/http';
 
 @Pipe({
   name: "convert"
 })
 export class ConvertPipe implements PipeTransform {
+  conversionRate: any;
+  constructor(private http: HttpClient) {
+    this.http.get('https://api.exchangeratesapi.io/latest?base=USD').subscribe(rate => {
+      this.conversionRate = JSON.stringify(rate["rates"]["INR"])
+      console.log(this.conversionRate)
+    });
+  }
+
   transform(value: any): any {
     let newStr: number;
 
     if (value == "NaN" || value <= 0) {
       newStr = 0;
     } else {
-      newStr = value * 71.55;
+      newStr = value * this.conversionRate;
     }
     return newStr;
   }
 
 }
+
